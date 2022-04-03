255c2da6c21f045c3f42772652fe3034f03d4174
nikki <nikkigoyal107@gmail.com>
2022-04-03 19:19:42 +0545

    lastestststtsts newww

--- resources/views/BackEnd/script.blade.php
+++ resources/views/BackEnd/script.blade.php
@@ -1,20 +1,29 @@
-<script src="{{asset('BackEnd/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('Backend/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('Backend/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{ asset('Backend/js/demo.js')}}"></script>
+
+{{-- <script src="{{asset('BackeBackEnd/plugins/jquery/jquery.min.js')}}"></script> --}}
 <!-- Bootstrap 4 -->
-<script src="{{asset('BackEnd/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('Backend/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('BackEnd/js/adminlte.min.js')}}"></script>
-<script src="{{asset('BackEnd/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('BackEnd/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{asset('BackEnd/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{asset('BackEnd/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
-<script src="{{asset('BackEnd/plugins/chart.js/Chart.bundle.js')}}"></script>
-<script src="{{asset('BackEnd/plugins/chart.js/Chart.bundle.min.js')}}"></script>
-<script src="{{asset('BackEnd/plugins/chart.js/Chart.css')}}"></script>
-<script src="{{asset('BackEnd/plugins/chart.js/Chart.js')}}"></script>
-<script src="{{asset('BackEnd/plugins/chart.js/Chart.min.css')}}"></script>
-<script src="{{asset('BackEnd/plugins/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('Backend/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('Backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('Backend/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('Backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+
+{{-- <script src="{{asset('Backend/plugins/chart.js/Chart.css')}}"></script> --}}
+{{-- <script src="{{asset('Backend/plugins/chart.js/Chart.js')}}"></script> --}}
+{{-- <script src="{{asset('Backend/plugins/chart.js/Chart.min.css')}}"></script> --}}
+<script src="{{asset('Backend/plugins/chart.js/Chart.min.js')}}"></script>
+{{-- <script src="{{asset('Backend/plugins/chart.js/Chart.bundle.js')}}"></script> --}}
+{{-- <script src="{{asset('Backend/plugins/chart.js/Chart.bundle.min.js')}}"></script> --}}
+<script src="{{asset('Backend/plugins/summernote/summernote-bs4.min.js')}}"></script>
+
 <!-- sweet alert -->
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="{{asset('Backend/js/adminlte.min.js')}}"></script>
+<script src="{{ asset('Backend/js/pages/dashboard.js')}}"></script>
+
 <script>
     $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
 
